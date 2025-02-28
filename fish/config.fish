39e633f21f61febbf5d3f084b6e7b6a44e4a9fcb
Tom Auty <tom@truex.co>
2025-02-28 18:04:24 -0500

    Clean up fish a bit

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,11 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-source ~/.fish_aliases
-source ~/.fish_variables
+alias x "exit"
+alias c "clear"
+alias gs "git status"
+alias gdc "git diff --cached"
+alias gd "git diff"
+
 fish_vi_key_bindings
 set fish_greeting
