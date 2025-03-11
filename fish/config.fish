599280e1c82e88ab452be3a274e70bae4dd6de8c
Tom Auty <tom.auty@gmail.com>
2025-03-11 14:56:07 -0400

    Tmux and everforest updates

--- fish/config.fish
+++ fish/config.fish
@@ -2,13 +2,20 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-alias v "nvim"
-alias vim "nvim"
-alias x "exit"
-alias c "clear"
+alias v nvim
+alias vim nvim
+alias x exit
+alias c clear
 alias gs "git status"
 alias gdc "git diff --cached"
 alias gd "git diff"
 
+function jcurl
+    curl $argv | jq
+end
+
 fish_vi_key_bindings
 set fish_greeting
+
+set -gx ANDROID_HOME ~/Library/Android/sdk
+set -gx PATH $ANDROID_HOME/emulator $ANDROID_HOME/tools $ANDROID_HOME/tools/bin $ANDROID_HOME/platform-tools $PATH
--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -1,11 +1,15 @@
 return {
-	{
-		"sainnhe/everforest",
-		lazy = false,
-		priority = 1000, -- make sure to load this before all the other start plugins
-		config = function()
-			-- load the colorscheme here
-			vim.cmd([[colorscheme everforest]])
-		end,
-	},
+  {
+    "sainnhe/everforest",
+    lazy = false,
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      -- load the colorscheme here
+      -- Optionally configure and load the colorscheme
+      -- directly inside the plugin declaration.
+      vim.g.everforest_enable_italic = true
+      vim.g.everforest_background = "hard"
+      vim.cmd([[colorscheme everforest]])
+    end,
+  },
 }
